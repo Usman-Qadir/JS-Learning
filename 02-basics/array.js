78242d9987afc912d995ce54c150d293671d5043
Usman Qadir <usman.qadir.cps@gmail.com>
2025-03-08 05:14:20 +0500

    till arrow functions

---
+++ 02-basics/array.js
@@ -0,0 +1,48 @@
+const myArr = [1, 2, 3, 4, 5]
+const myHeros =["AbuBakr", "Umer", "Usman", "Ali"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+//console.log(myArr)
+
+
+//* Array Methods 
+//myArr.push(6)
+// myArr.pop()
+// myArr.unshift(0)
+// myArr.shift()
+ 
+// const myArr3 = myArr.join()
+// console.log(myArr3)
+// console.log(myArr.indexOf(3))
+// console.log(myArr.includes(3))
+
+// console.log(typeof myArr3)
+
+// const  myNewArr = myArr.slice(1, 3)
+// console.log(myArr)
+// console.log("A = ",myNewArr)
+// console.log(myArr)
+
+// const myNewArr2 = myArr.splice(1, 3)
+// console.log(myArr)
+// console.log("B = ",myNewArr2)
+
+// ******************************************************************
+
+// const marvel_heros = ["thor", "ironman", "captain america", "black widow", "hulk"]
+// const dc_heros = ["superman", "batman", "flash", "aquaman"]
+
+// const all_heros = marvel_heros.concat(dc_heros)
+// console.log(all_heros)
+
+// const anotherArr = [1, 2, 3, [4, 5,6],7, [6,7,[4,5]]]
+// const realArr = anotherArr.flat(Infinity)
+// console.log(realArr)
+
+ 
+//*****************************************************************************
+// console.log(Array.isArray("Usman Qadir"))
+// console.log(Array.from("Usman Qadir"))
+// console.log(Array.from({name:"Usman", age: 23})) // uinteresting scenario
+
+
